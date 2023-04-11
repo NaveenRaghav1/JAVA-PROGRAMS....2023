2effca47275d202fce958fc7507bf43ccf916172
Naveen Raghav <122202570+NaveenRaghav1@users.noreply.github.com>
2023-04-11 21:14:29 +0530

    Add files via upload

---
+++ 1.java
@@ -0,0 +1,15 @@
+package com.company;
+
+class first{
+
+
+    public static void main(String args[]){  
+        System.out.println("hello world");
+        
+            System.out.println("jai shree ram");
+            
+            System.out.println("jai sita ram");
+
+        }
+}
+
