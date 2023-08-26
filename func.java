7bea7736ec8cf555f5ef8e5ec18f26a386953e7c
Naveen Raghav <122202570+NaveenRaghav1@users.noreply.github.com>
2023-08-26 21:39:26 +0530

    Add files via upload

---
+++ func.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class func {
+    public class sum{
+        public static void main(String[] args) {
+            sum();
+            sum();
+        }
+        static void sum(){
+         System.out.println("hello naveen");
+        Scanner sum=new Scanner(System.in);
+        System.out.println("enter first number-->");
+        int num1=sum.nextInt();
+        System.out.println("enter second number-->");
+        int num2=sum.nextInt();
+        int sume=num1+num2;
+        System.out.println("the sum of the numbers is-->"+ sume);
+        }
+    }
+    }
+}
