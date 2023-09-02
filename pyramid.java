d7a9ea6cb338bd7303eea7585bd509608e97057a
Naveen Raghav <122202570+NaveenRaghav1@users.noreply.github.com>
2023-09-02 22:28:02 +0530

    Add files via upload

---
+++ pyramid.java
@@ -0,0 +1,14 @@
+public class pyramid {
+    public static void main(String[] args) {
+        for(int i=0;i<5;i++){
+            for(int j=i;j<5;j++){
+            System.out.print(" ");
+            for(int k=0;k<=(2*i-1);j++){
+                System.out.print("*");
+            }
+         }
+         System.out.println();
+        }
+        }
+    }
+
