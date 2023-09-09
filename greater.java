689fa5c1b6ec8519be34492d098f586598fe6347
Naveen Raghav <122202570+NaveenRaghav1@users.noreply.github.com>
2023-09-09 09:18:59 +0530

    Add files via upload

---
+++ greater.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class greater {
+    public static void main(String[]args) {
+        int b=45;
+        Scanner a =new Scanner(System.in);
+        System.out.println("enter a number");
+        int c=a.nextInt();
+        if (b>c){
+        System.out.println("given number is greater");}
+        else{
+        System.out.println("usesr input number is greater");
+    }
+}
+}
