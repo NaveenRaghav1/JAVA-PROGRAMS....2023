ed6b7a40ffc5421bc1afcb86973434ad2701e9f5
Naveen Raghav <122202570+NaveenRaghav1@users.noreply.github.com>
2023-09-11 09:10:24 +0530

    Add files via upload

--- sum.java
+++ sum.java
@@ -1,5 +1,23 @@
-public static void main(String[] args) {
-   System.out.print("helloo naveen");
-       
-}
+/*import java.util.Scanner;
+public class sum {
+    public static void main(String[]args){
+        System.out.println("hello user");
+Scanner k=new Scanner(System.in);
+System.out.println("enter first number--");
+int a=k.nextInt();
+System.out.println("enter second number--");
+int b=k.nextInt();
+int sum=a+b;
+System.out.println("the sum of the given number is");
+System.out.println(sum); 
 
+
+}
+}*/
+public class sum {
+    public static void main(String[]args){
+        System.out.println("hello user");
+        float a=7/4*9/2;
+        System.out.println(a);
+    }
+}
