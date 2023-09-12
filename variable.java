8e130a630e54034253466331bdb111f2cad80012
Naveen Raghav <122202570+NaveenRaghav1@users.noreply.github.com>
2023-09-12 21:09:09 +0530

    Add files via upload

---
+++ variable.java
@@ -0,0 +1,15 @@
+package day1;
+public class variable {
+    public static void main(String[]args) {
+        System.out.println("hello payal ji");
+        int a=20;
+        double b=34;
+        float c=344;
+        String name ="aniket";
+System.out.println(a);
+System.out.println(b);
+System.out.println(c);
+System.out.println(name);
+
+    }
+}
