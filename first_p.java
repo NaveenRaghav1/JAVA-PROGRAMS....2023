eca4a28ff9142e1153c5208713d60c087f72d55d
Naveen Raghav <122202570+NaveenRaghav1@users.noreply.github.com>
2023-09-16 13:24:45 +0530

    Add files via upload

---
+++ first_p.java
@@ -0,0 +1,16 @@
+package day1;
+class first_p{
+
+
+    public static void main(String args[]){  
+        System.out.println("hello world");
+        
+            System.out.println("jai shree ram");
+            
+            System.out.println("jai sita ram");
+            System.out.println("hello moto");
+
+        }
+}
+
+
