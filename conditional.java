b1d20723826333ed3f3f066e11a4a1a0f758800f
Naveen Raghav <122202570+NaveenRaghav1@users.noreply.github.com>
2023-09-17 12:37:40 +0530

    Add files via upload

---
+++ conditional.java
@@ -0,0 +1,19 @@
+package day1;
+public class conditional {
+    public static void main(String[]args) {
+System.out.println("hello ji \n we cover conditional statements of java like\n if....else");        
+   int age=23;
+   if(age>18){
+   System.out.println("you are adult");
+    }
+   else{
+    System.out.println("you are not adult");
+   }
+
+
+
+
+
+
+}
+}
