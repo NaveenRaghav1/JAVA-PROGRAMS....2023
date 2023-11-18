72c70ed55c3dff64faf4676a1b396c6e78236edc
Naveen Raghav <122202570+NaveenRaghav1@users.noreply.github.com>
2023-11-18 14:15:45 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,6 @@
+//a simple java class and hello world program date 17/11/23
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world with naveen raghav");
+    }
+}
