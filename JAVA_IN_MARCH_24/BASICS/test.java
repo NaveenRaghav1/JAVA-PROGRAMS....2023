5ee6cc24d69b34f2f57b35ec485e87bab15e59ee
Naveen Raghav <122202570+NaveenRaghav1@users.noreply.github.com>
2024-03-10 20:57:32 +0530

    Add files via upload

--- JAVA_IN_MARCH_24/BASICS/test.java
+++ JAVA_IN_MARCH_24/BASICS/test.java
@@ -1,9 +1,23 @@
+import java.lang.reflect.Array;
+import java.util.Scanner;
+
 public class test {
-     int x;
-    public static void main(String[] args){
-        System.out.println(fun());
+    public static void main(String[] args) {
+        System.out.println("hello--naveen--");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the size of array");
+        int n = sc.nextInt();
+        System.out.println(n);
+        //orderOf(arr[n]);
     }
-    static int fun(){
-        return x;
+
+    int orderOf(int arr[n]){
+        Arrays.sort(arr);
+        int ans = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int temp = arr[i] * (arr.length - i);
+            ans = ans + temp;
+        }
+        return ans;
     }
 }
