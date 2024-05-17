84c23ac2129a77f70ee6ac02ea9508650e7f86dc
Naveen Raghav <122202570+NaveenRaghav1@users.noreply.github.com>
2024-05-17 23:29:25 +0530

    Add files via upload

--- test.java
+++ test.java
@@ -1,23 +1,9 @@
-import java.lang.reflect.Array;
-import java.util.Scanner;
+package constructor;
+//package OOPSCONCEPT;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println("hello--naveen--");
-        Scanner sc = new Scanner(System.in);
-        System.out.println("enter the size of array");
-        int n = sc.nextInt();
-        System.out.println(n);
-        //orderOf(arr[n]);
+        System.out.println("hello naveen");
     }
 
-    int orderOf(int arr[n]){
-        Arrays.sort(arr);
-        int ans = 0;
-        for (int i = 0; i < arr.length; i++) {
-            int temp = arr[i] * (arr.length - i);
-            ans = ans + temp;
-        }
-        return ans;
-    }
 }
